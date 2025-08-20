57b1ac1dc6593ec287e1e341fae44afedfd6f338
Hackrband <138342638+hackrband@users.noreply.github.com>
2025-08-20 15:03:54 -0400

    Update Day2.py

---
+++ Day2.py
@@ -0,0 +1,16 @@
+#Day 2 | HackerRank 30 Days of Code
+
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+# Read input values
+i2 = int(input().strip())
+d2 = float(input().strip())
+s2 = input().strip()
+
+# Perform operations
+print(i + i2)
+print(d + d2)
+print(s + s2)
+
